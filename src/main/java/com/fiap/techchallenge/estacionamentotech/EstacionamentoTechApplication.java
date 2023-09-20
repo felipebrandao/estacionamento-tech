3122e75cc95a95dc70032b1f05682fac4a8f6a06
Felipe Brandão <felipe.b31@gmail.com>
2023-09-20 12:09:45 -0300

    arquivos iniciais

---
+++ src/main/java/com/fiap/techchallenge/estacionamentotech/EstacionamentoTechApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge.estacionamentotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamentoTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionamentoTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/techchallenge/estacionamentotech/EstacionamentoTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge.estacionamentotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamentoTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
